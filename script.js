9b4ffa9338ab647a68bcf566a8da7e76665b7b7e
Sean Jakobs <seanjakobs@gmail.com>
2020-03-08 16:36:17 -0700

    added button to create clear grid. Also did minor refactoring

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const container = document.getElementById('container');
 const prompt = 256;
 
-for ( i = 0; i < prompt; i++){
-    let div = document.createElement('div');
-    div.setAttribute('class', 'gridDiv');
-    div.addEventListener('mouseover', blockSelected);
-    container.appendChild(div);
+function createGrid() {
+    for ( i = 0; i < prompt; i++){
+        let div = document.createElement('div');
+        div.setAttribute('class', 'gridDiv');
+        div.addEventListener('mouseover', blockSelected);
+        container.appendChild(div);
+    }
+}
+
+const clear = document.getElementById("clear-button");
+clear.addEventListener('click', clearGrid);
+
+// To clear and repopulate the grid
+function clearGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+    createGrid();
 }
 
 function blockSelected() {
     this.classList.add('block-selected');
 }
+
+// To create grid on page load
+createGrid();
