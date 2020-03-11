7be147ca68c79fa4c3144ab5f0b42fa57e1baf2a
Sean Jakobs <seanjakobs@gmail.com>
2020-03-10 18:53:40 -0700

    restructured grid to feature each block within its own row div

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const container = document.getElementById('container');
+const row = document.getElementsByClassName('gridRow');
 let gridSize = 16;
 
 
-function createGrid() {
-    let gridSpaces = (gridSize * gridSize);
-    for ( i = 0; i < gridSpaces; i++){
+
+
+
+function createRow() {
+    for ( i = 0; i < gridSize; i++){
         let div = document.createElement('div');
-        div.setAttribute('class', 'gridDiv');
-        div.addEventListener('mouseover', blockSelected);
+        div.setAttribute('class', 'gridRow');
         container.appendChild(div);
     }
 }
+
+function populateRow(){
+    for ( i = 0; i < gridSize; i++){
+        [...row].forEach((parent, i) => {
+            let div = document.createElement('div');
+            div.setAttribute('class', 'gridDiv');
+            div.addEventListener('mouseover', blockSelected);
+            parent.appendChild(div);
+        });
+    }
+}
+
+function createGrid(){
+    createRow();
+    populateRow();
+}
+
 
 const clear = document.getElementById("clear-button");
 clear.addEventListener('click', clearGrid);
